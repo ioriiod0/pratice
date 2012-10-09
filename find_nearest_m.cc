8f68a331188d8470328bbfa380011909c68d699d
磊 高 <ioriiod0@raitekiMacBook-Air.local>
2012-10-10 02:15:42 +0800

    add find_nearest_n.cc

---
+++ find_nearest_m.cc
@@ -0,0 +1,57 @@
+#include <iterator>
+#include <map>
+#include <set>
+#include <utility>
+
+template <typename BST,typename Key>
+std::pair<BST::iterator,BST::iterator> find_nearest_m(const BST& bst,const Key& key,int m)
+{
+	if(bst.size() <= m)
+		return std::make_pair(bst.begin(),bst.end());
+
+	BST::iterator r = bst.lower_bound(key); //KEY右面的游标
+	BST::iterator l = std::advance(r,-1); //KEY左面的游标
+	
+	while( m > 0 && r != bst.end() && l != bst.rend() )
+	{
+		if(l->first - key < key - r->first)//右面比较靠近
+		{
+			++r;
+			m-=1;
+		}
+		else if( it->first - key > key - j->first) //左面比较近
+		{
+			--l;
+			m-=1;
+		}
+		else //两边一样近
+		{
+			++r;
+			--l;
+			m-=2;
+		}
+ 
+	}
+
+	if(m == 0)
+	{
+		return std::make_pair(l++,r)
+	}
+	else if(l != bst.rend()) //m不为0，且左面不空，
+	{
+		return std::make_pair(std::advance(l,1-m),r);
+	}
+	else //m不为0，且右面不为空。
+	{
+		return std::make_pair(l,std::advance(r,m));
+	}
+
+} 
+
+
+int int main(int argc, char const *argv[])
+{
+	
+	
+	return 0;
+}
